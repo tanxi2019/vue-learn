397bf1844405724ea17232407de7962086e37cfd
风很大 <1832355298@qq.com>
2019-09-28 09:43:18 +0800

    9/28

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-table :data="tableData.slice((currentPage-1)*pagesize,currentPage*pagesize)" style="width: 100%">
+    <el-table :data="tableData.slice((currentPage-1)*pagesize,currentPage*pagesize)"  v-loading="loading" style="width: 100%">
       <el-table-column prop="id" label="日期" width="180">
       </el-table-column>
       <el-table-column prop="name" label="姓名" width="180">
@@ -16,7 +16,7 @@
         :page-sizes="[5,10, 20, 30, 40]"
         :page-size="pagesize"
         background
-        layout="total, sizes,prev, pager, next, jumper"
+        layout="total, sizes, prev, pager, next, jumper"
         :total="tableData.length"
         prev-text="上一页"
         next-text="下一页">
@@ -32,7 +32,8 @@ export default {
     return {
       currentPage: 1, // 默认显示页面为1
       pagesize: 5, //    每页的数据条数
-      tableData: [] // 需要data定义一些，tableData定义一个空数组，请求的数据都是存放这里面
+      tableData: [], // 需要data定义一些，tableData定义一个空数组，请求的数据都是存放这里面
+      loading: false
     }
   },
   mounted () {
@@ -43,6 +44,10 @@ export default {
       axios.get('http://localhost:3000/posts').then(response => {
         console.log(response.data)
         this.tableData = response.data
+        this.loading = true
+        setTimeout(() => {
+          this.loading = false
+        }, 2000)
       })
     },
     // 每页下拉显示数据
@@ -52,6 +57,10 @@ export default {
     // 点击第几页
     handleCurrentChange: function (currentPage) {
       this.currentPage = currentPage
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+      }, 2000)
     }
 
   }
