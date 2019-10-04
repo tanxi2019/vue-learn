3dcac2ebd25e1eaa4e5939cc1d4ea3d80e45dace
unknown <1832355298@qq.com>
2019-10-04 21:05:41 +0800

    0000

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -14,60 +14,56 @@
 </template>
 
 <script>
-  import axios from 'axios'
-  export default {
-    data() {
-      return {
-        userName:'',
-        passWord:''
-      }
-    },
-    methods: {
-      login:function(){ // 登陆验证
-        if(this.userName===''||this.passWord===''){
-          this.$message({
-            message: '账号密码不能为空！',
-            type: 'warning'
-          });
-          return;
-        }
-        let  parms = {
-          userName:this.userName,
-          passWord:this.passWord
-        }
-        axios.get('http://localhost:3000/users',{parms:parms}).then((res)=>{
-          if(res.data.length>0){
-            var ses = window.sessionStorage;
-            var d = res.data[0].username
-            // 把拿到的data放在sessionStorage中
-            ses.setItem('msg',d)
-            // 验证成功进入首页
-            this.$router.push('/');
-            // 登陆成功
-            window.location.reload()
-          }else{
-            this.$message({
-              message: '请输入正确的账号密码！',
-              type: 'warning'
-            });
-          }
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      userName: '',
+      passWord: ''
+    }
+  },
+  methods: {
+    login: function () { // 登陆验证
+      if (this.userName === '' || this.passWord === '') {
+        this.$message({
+          message: '账号密码不能为空！',
+          type: 'warning'
         })
+        return
       }
+      let parms = {
+        userName: this.userName,
+        passWord: this.passWord
+      }
+      axios.get('http://localhost:3000/users', {parms: parms}).then((res) => {
+        if (res.data.length > 0) {
+          var ses = window.sessionStorage
+          var d = res.data[0].username
+          // 把拿到的data放在sessionStorage中
+          ses.setItem('msg', d)
+          // 验证成功进入首页
+          this.$router.push('/')
+          // 登陆成功
+          window.location.reload()
+        } else {
+          this.$message({
+            message: '请输入正确的账号密码！',
+            type: 'warning'
+          })
+        }
+      })
     }
   }
+}
 </script>
 
 <style scoped>
   .login-container {
-    -webkit-border-radius: 5px;
     border-radius: 5px;
-    -moz-border-radius: 5px;
     background-clip: padding-box;
     margin: 180px auto;
     width: 350px;
     padding: 35px 35px 15px 35px;
-    /*background: #fff;*/
-    /*border: 1px solid #eaeaea;*/
     box-shadow: 0 0 25px #cac6c6;
   }
     .title {
