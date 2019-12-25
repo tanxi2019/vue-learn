6886ed9e3d92d464299c858dba80ac839e004ee9
风很大 <1832355298@qq.com>
2019-12-25 13:16:48 +0800

    12.25

--- Swiper.vue
+++ Swiper.vue
@@ -4,6 +4,7 @@
     <swiper-slide v-for="(banner,i) in banners" :key="i">
       <img class='boxitem' :src="banner" alt="">
       </swiper-slide>
+
     <!-- Optional controls -->
     <div class="swiper-pagination"  slot="pagination"></div>
     <div class="swiper-button-prev" slot="button-prev"></div>
@@ -29,29 +30,25 @@ export default {
         require('../assets/banner2.jpg'),
         require('../assets/banner3.jpg')
       ],
-      swiperOption: { // 以下配置不懂的，可以去swiper官网看api
+      swiperOption: {
         effect: 'flip', // 切换效果 fade,coverflow,cube，flip;
-        notNextTick: true, // notNextTick是一个组件自有属性；
         autoplay: true, // 自动播放
-        loop: true, // 循环播放
-        direction: 'horizontal',
-        grabCursor: true,
-        setWrapperSize: true,
-        autoHeight: true,
+        loop: true, //循环播放
+        delay: 3000, // 延迟3s
+        speed: 800, //
+        keyboard: true, // 键盘
+        freeMode: false,
         pagination: { // 分页器
-          el: '.swiper-pagination'
+          el: '.swiper-pagination',
+          dynamicBullets: false, // 手动切换之后继续自动轮播
+          clickable: true // 允许点击小圆点跳转
         },
-        centeredSlides: true,
-        paginationClickable: true,
-        navigation: { // 左右切换箭头
+        navigtion: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev'
         },
-        keyboard: true, // 键盘切换
-        mousewheelControl: true,
-        observeParents: true, // 如果自行设计了插件，那么插件的一些配置相关参数，也应该出现在这个对象中，如下debugger
-        debugger: true
-      },
+        on: {}
+      }
     }
   },
   methods: {}
@@ -59,6 +56,5 @@ export default {
 </script>
 
 <style scoped>
-  .swiper-container{ width: 100%; overflow: hidden;}
   .swiper-container .swiper-button-hidden{ opacity : 0; }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import animate from 'animate.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper /* { default global options } */)
 
 Vue.use(animate)
 Vue.use(ElementUI)
